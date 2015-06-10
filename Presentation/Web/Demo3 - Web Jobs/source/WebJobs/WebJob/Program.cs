8af0ce44e9eafe803d916f0dff217ba4318c6a16
nazik <nazikhuq@yahoo.com>
2015-06-10 09:11:46 -0400

    [Web Apps] #45 Added Web Jobs demo
    Added Web Jobs demo project.

---
+++ Presentation/Web/Demo3 - Web Jobs/source/WebJobs/WebJob/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace TableOperations
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            CreateDemoData();
+
+            JobHost host = new JobHost();
+            Task callTask = host.CallAsync(typeof(Functions).GetMethod("ManualTrigger"));
+            
+            Console.WriteLine("Waiting for async operation...");
+            callTask.Wait();
+            Console.WriteLine("Task completed: " + callTask.Status);
+
+            // host.RunAndBlock();
+        }
+
+        private static void CreateDemoData()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("textinput");
+            queue.CreateIfNotExists();
+
+            queue.AddMessage(new CloudQueueMessage("Hello hello world"));
+        }
+    }
+}
